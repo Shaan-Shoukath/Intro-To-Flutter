3f64489617de9e366c53127604ee6cf26c845f7f
Shaan-Shoukath <shaanshoukath4522@gmail.com>
2026-02-10 17:32:44 +0530

    All Final Materials Added

---
+++ Test/intro_to_flutter/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Simple Counter App',
+      home: CounterWidget(),
+    );
+  }
+}
+
+class CounterWidget extends StatefulWidget {
+  @override
+  State<CounterWidget> createState() => _CounterWidgetState();
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int count = 0;
+
+  void increment() {
+    setState(() {
+      count++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 83, 132, 229),
+      appBar: AppBar(
+        title: Text(
+          'Counter Example',
+          selectionColor: Color.fromARGB(255, 255, 255, 255),
+        ),
+        backgroundColor: Color.fromARGB(255, 0, 46, 137),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Count: $count', style: TextStyle(fontSize: 24)),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: increment,
+              child: Text(
+                'Hello Flutter',
+                style: GoogleFonts.poppins(
+                  fontSize: 24,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
